8fbf1789235684c073d97038bb371ae567ee0dd5
Fabrizio Perria <fabrizio.perria@gmail.com>
2025-09-24 15:39:36 +0200

    Push and pop

--- plugin/source/CircularBuffer.h
+++ plugin/source/CircularBuffer.h
@@ -8,12 +8,29 @@ template <typename FloatType>
 class CircularBuffer
 {
 public:
-    explicit CircularBuffer (size_t capacityPow2) : capacity (nextPowerOfTwo (capacityPow2)), mask (capacity - 1), write (0)
+    explicit CircularBuffer (size_t capacityPow2) : capacity (nextPowerOfTwo (capacityPow2)), mask (capacity - 1), write (0), read (0)
     {
         buffer.assign (capacity, FloatType {});
     }
 
-    void pushBlock (const FloatType* in, size_t numSamples) {}
+    void pushBlock (const FloatType* in, size_t numSamples)
+    {
+        for (size_t i = 0; i < numSamples; ++i)
+        {
+            buffer[write] = in[i];
+            write = (write + 1) & mask;
+        }
+        snapshot.store (write, std::memory_order_release);
+    }
+
+    void popBlock (FloatType* output, size_t numSamples)
+    {
+        for (size_t i = 0; i < numSamples; ++i)
+        {
+            output[i] = buffer[read];
+            read = (read + 1) & mask;
+        }
+    }
 
     void copyForGui (std::vector<FloatType>& dest) const {}
     size_t getCapacity() const noexcept { return capacity; }
@@ -22,6 +39,7 @@ private:
     std::vector<FloatType> buffer;
     size_t capacity, mask;
     size_t write;
+    size_t read;
     std::atomic<size_t> snapshot { 0 };
 
     static size_t nextPowerOfTwo (size_t v) noexcept
--- test/source/CircularBuffer_test.cpp
+++ test/source/CircularBuffer_test.cpp
@@ -38,4 +38,68 @@ TEST (CircularBufferTest, AllocateInPowerOfTwo)
     }
 }
 
+TEST (CircularBufferTest, PushPop)
+{
+    CircularBuffer<float> buffer (8); // capacity 8
+    std::vector<float> out (8, 0.0f);
+
+    std::vector<float> in1 { 1.0f, 2.0f, 3.0f };
+    buffer.pushBlock (in1.data(), in1.size());
+
+    buffer.popBlock (out.data(), in1.size());
+    EXPECT_EQ (out[0], 1.0f);
+    EXPECT_EQ (out[1], 2.0f);
+    EXPECT_EQ (out[2], 3.0f);
+
+    std::vector<float> in2 { 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f };
+    buffer.pushBlock (in2.data(), in2.size());
+
+    buffer.popBlock (out.data(), in2.size());
+    EXPECT_EQ (out[0], 4.0f);
+    EXPECT_EQ (out[1], 5.0f);
+    EXPECT_EQ (out[2], 6.0f);
+    EXPECT_EQ (out[3], 7.0f);
+    EXPECT_EQ (out[4], 8.0f);
+    EXPECT_EQ (out[5], 9.0f);
+
+    std::vector<float> in3 { 10.0f, 11.0f, 12.0f, 13.0f };
+    buffer.pushBlock (in3.data(), in3.size());
+
+    buffer.popBlock (out.data(), in3.size());
+    EXPECT_EQ (out[0], 10.0f);
+    EXPECT_EQ (out[1], 11.0f);
+    EXPECT_EQ (out[2], 12.0f);
+    EXPECT_EQ (out[3], 13.0f);
+}
+
+TEST (CircularBufferTest, WrapAround)
+{
+    CircularBuffer<float> buffer (4); // capacity 4
+    std::vector<float> out (4, 0.0f);
+
+    std::vector<float> in1 { 1.0f, 2.0f, 3.0f };
+    buffer.pushBlock (in1.data(), in1.size());
+
+    buffer.popBlock (out.data(), in1.size());
+    EXPECT_EQ (out[0], 1.0f);
+    EXPECT_EQ (out[1], 2.0f);
+    EXPECT_EQ (out[2], 3.0f);
+
+    std::vector<float> in2 { 4.0f, 5.0f, 6.0f };
+    buffer.pushBlock (in2.data(), in2.size());
+
+    buffer.popBlock (out.data(), in2.size());
+    EXPECT_EQ (out[0], 4.0f);
+    EXPECT_EQ (out[1], 5.0f);
+    EXPECT_EQ (out[2], 6.0f);
+
+    std::vector<float> in3 { 7.0f, 8.0f, 9.0f };
+    buffer.pushBlock (in3.data(), in3.size());
+
+    buffer.popBlock (out.data(), in3.size());
+    EXPECT_EQ (out[0], 7.0f);
+    EXPECT_EQ (out[1], 8.0f);
+    EXPECT_EQ (out[2], 9.0f);
+}
+
 } // namespace audio_plugin_test
