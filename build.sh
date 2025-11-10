e87df49d40b91786298bcffed39bbd343ca0c91c
Fabrizio Perria <fabrizio.perria@gmail.com>
2025-11-10 17:34:32 +0100

    Fix coverage version mismatch

--- build.sh
+++ build.sh
@@ -8,7 +8,10 @@ PROFILE_FILE="${PWD}/${BUILD_DIR}/coverage-%p.profraw"
 MERGED_FILE="${BUILD_DIR}/coverage.profdata"
 HTML_DIR="${BUILD_DIR}/coverage_html"
 
-cmake -S . -B "$BUILD_DIR" -DENABLE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B "$BUILD_DIR" -DENABLE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_C_COMPILER=/opt/homebrew/opt/llvm/bin/clang \
+    -DCMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++
+
 cmake --build "$BUILD_DIR" -j8
 
 export LLVM_PROFILE_FILE="$PROFILE_FILE"
@@ -16,15 +19,15 @@ cd "$BUILD_DIR"
 ctest --output-on-failure
 cd ..
 
-# rm -rf ${MERGED_FILE}
-# llvm-profdata merge -sparse "$BUILD_DIR"/coverage-*.profraw -o "$MERGED_FILE"
-# rm -rf ${BUILD_DIR}/*.profraw
-#
-# llvm-cov show "$TEST_TARGET_DIR" \
-#     -instr-profile="$MERGED_FILE" \
-#     -format=html \
-#     -output-dir="$HTML_DIR" \
-#     --show-branches=percent \
-#     $(find "$PWD/plugin/source" -name '*.cpp' -o -name '*.h')
-#
-# echo "Coverage report generated at $HTML_DIR/index.html"
+rm -rf ${MERGED_FILE}
+llvm-profdata merge -sparse "$BUILD_DIR"/coverage-*.profraw -o "$MERGED_FILE"
+rm -rf ${BUILD_DIR}/*.profraw
+
+llvm-cov show "$TEST_TARGET_DIR" \
+    -instr-profile="$MERGED_FILE" \
+    -format=html \
+    -output-dir="$HTML_DIR" \
+    --show-branches=percent \
+    $(find "$PWD/plugin/source" -name '*.cpp' -o -name '*.h')
+
+echo "Coverage report generated at $HTML_DIR/index.html"
